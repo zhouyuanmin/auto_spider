0a63be2ac637a054372927b1d648856dcba473ac
zhouyuanmin <1837722596@qq.com>
2023-10-30 12:15:00 +0800

    GSAGood

--- goods/models.py
+++ goods/models.py
@@ -25,10 +25,26 @@ class BaseModel(models.Model):
 
 
 class SynnexGood(BaseModel):
-    manufacturer_name = models.CharField(max_length=255, default="", verbose_name="company-name")  # 厂家名称
+    mfr = models.CharField(max_length=255, default="", verbose_name="Mfr")  # 厂家名称
     mfr_p_n = models.CharField(max_length=255, default="", verbose_name="Mfr.P/N")  # 产品编号 等价于part_number
     sku = models.CharField(max_length=255, default="", verbose_name="SKU")
     td_snx = models.CharField(max_length=255, default="", verbose_name="TD SNX#")
     msrp = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="MSRP")  # 建议零售价
     federal_govt_price = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="联邦政府价格")
     status = models.BooleanField(null=True, default=None, verbose_name="状态")  # True产品存在、False产品不存在、None未爬
+
+
+class GSAGood(BaseModel):
+    # 列表页
+    mfr_part_number = models.CharField(max_length=255, default="", verbose_name="mfrPartNumber")  # 产品编号 等价于part_number
+    product_name = models.CharField(max_length=255, default="", verbose_name="itemName")  # 产品名称
+    mfr = models.CharField(max_length=255, default="", verbose_name="Mfr")  # 厂家名称
+    source = models.IntegerField(verbose_name="source")
+    url = models.CharField(max_length=255, default="", verbose_name="url")
+    # 详情页
+    mas_sin = models.CharField(max_length=255, default="", verbose_name="MAS Schedule/SIN")  # 33411
+    coo = models.CharField(max_length=255, default="", verbose_name="原产地")
+    description = models.CharField(max_length=2047, default="", verbose_name="产品描述")
+    gsa_price_1 = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="GSA优势价格1")
+    gsa_price_2 = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="GSA优势价格2")
+    gsa_price_3 = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="GSA优势价格3")
