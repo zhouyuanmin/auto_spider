8c75f9d917ff012ccbcd4bfc88991cc4dafc1bdc
zhouyuanmin <1837722596@qq.com>
2023-11-01 17:44:42 +0800

    debug

--- client/main.py
+++ client/main.py
@@ -480,7 +480,8 @@ def refresh_gsa_good(part_number, browser):
 
         source_divs = product_div.find_elements_by_xpath(page_elements.get("sources"))
         if not source_divs:  # 有些产品 没有sources
-            raise ValueError(f"有些产品没有sources part_number={part_number}")
+            logging.warning(f"有些产品没有sources part_number={part_number}")
+            # raise ValueError(f"有些产品没有sources part_number={part_number}")
         source_div = product_div.find_element_by_xpath(page_elements.get("sources"))
         source = text2source(source_div.text)
 
